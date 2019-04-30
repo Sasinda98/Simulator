c2aff518cd73598adede0841829bb2ce94d3c046
Gayal Rupasinghe <sasindarupasinghe@icloud.com>
2019-04-30 18:12:32 +0530

    adding the project files

---
+++ scheduler/main.c
@@ -0,0 +1,21 @@
+
+/* 
+ * File:   main.c
+ * Author: sasinda
+ *
+ * Created on 30 April 2019, 6:10 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("hello world");
+
+    return (EXIT_SUCCESS);
+}
+
