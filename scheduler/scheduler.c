35913bc969eafc45d58a4cf1090c989e877bfee4
Gayal Rupasinghe <sasindarupasinghe@icloud.com>
2019-05-01 13:13:22 +0530

    coding two task at a time function fetcher

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -10,20 +10,16 @@
 #include <stdlib.h>
 #include "readyqueue.h"
 
-
-
 int generateTaskFile(char *fileName);
 int readTaskFile(char *fileName);
-
-
-
+int getTwoTasks(char *fileName);
 
 int main(int argc, char** argv) {
     //File name and amount of tasks m is taken here.
     printf("Scheduler started!\n");
-    
-    struct Task tasks[2];
-    initialize(tasks, 2);
+   /*
+    struct Task tasks[5];
+    initialize(tasks, 3);
 
     struct Task ts1;
     ts1.task_number = 44;
@@ -40,14 +36,19 @@ int main(int argc, char** argv) {
     b = insert(ts2);
     c = insert(ts3);
     
-    printf("%d, %d, %d\n", a,b,c);
+    printf("%d, %d, %d\n", a,b,c); */ 
     
-    struct Task *out = pop();
+    while(isEmpty()==0){
+          struct Task *out = pop();
     printf("Popped task#= %d\n", out->task_number);
+    }
     
-    struct Task *out1 = pop();
-    printf("Popped task#= %d\n", out1->task_number);
     
+    for(int i=0; i<2; i++){
+        getTwoTasks("task_file");
+    }
+    
+   
     
    // int *a = (int *)malloc(sizeof(int)*3);
     //generateTaskFile("task_file");  //generates task file.
@@ -111,6 +112,46 @@ int readTaskFile(char *fileName){
     }
     
     
+    fclose(pFile);  //closing opened file
+    pFile = NULL; //making sure reference is not there anymore.
+    return 0;
+    
+}
+
+long fileReadHead;
+
+//Reads the taskfile 2 tasks at a time.
+int getTwoTasks(char *fileName){
+    
+    FILE *pFile = fopen(fileName, "r");    //open for writing.
+    
+    if(pFile == NULL){
+        char temp[3];
+        printf("Failed to open file, press any key followed by enter key to exit.");
+        scanf("%s", temp);
+        return -1;
+    }
+    
+    fseek(pFile, 0, SEEK_END);  //moving file position indicator to the end.
+   // printf("SIZE  %ld\n", ftell(pFile));    
+    long endOfFile = ftell(pFile);  //current position (i.e. eof) of file position indicator.
+    
+    if(fileReadHead != endOfFile){      //if file position indicatior 'fileReadHead' is not at the end of file.
+
+        fseek(pFile, fileReadHead, SEEK_SET);  //move file position indicator to last left off position.         
+
+        int task_number, cpu_burst;
+
+        for(int i = 0; i < 2; i++){
+            fscanf(pFile, "%d %d\n", &task_number, &cpu_burst);  //moves file position indicator by 2 lines, (for loop).
+            printf("%d %d\n", task_number, cpu_burst);
+        }
+    }else{
+        printf("END OF FILE REACHED\n");
+    }
+
+    fileReadHead = ftell(pFile);    //store the current position of file position indicator so the next time, it start read from there.
+
     fclose(pFile);  //closing opened file
     pFile = NULL; //making sure reference is not there anymore.
     return 0;
